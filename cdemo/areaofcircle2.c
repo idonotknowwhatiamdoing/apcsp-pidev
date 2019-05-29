02653bdf180a418783ff4a37eca61d0ea8e9ccd8
idonotknowwhatiamdoing <zhongshera@gmail.com>
2019-05-29 08:16:10 -0700

    committed cdemo

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -1,15 +1,40 @@
+#include <stdlib.h>
 #include <stdio.h>
- 
-int main() {
-   float radius, areaofcircle;
- 
-   printf("Enter the radius of circle : ");
-   scanf("%f", &radius);
 
-if ( radius > 3.5 && radius < 12.5) {
-   areaofcircle = 3.14 * radius * radius;
-   printf("Area of circle : %f", areaofcircle);
-} 
+ 
+float areaOfCircle(float radius)
+{
+float area = (3.14 * radius * radius);
+return area;
+}
 
-   return 0;
+ 
+int main(int argc, char* argv[])
+{
+
+	char input [100];
+	float lower;
+	float upper;
+
+	if (argc != 3)
+	{
+		printf("input lower and upper bounds\n");
+		while (1)
+		{
+			fgets(input, 100, stdin);
+			if (sscanf(input, "%f %f", &lower, &upper) == 2) break;
+			printf("Not a valid input - try again!\n");
+		}
+	}
+	else
+	{
+		lower = atof(argv[1]);
+		upper = atof(argv[2]); 
+	}
+
+	for (float radius = lower; radius <= upper; radius++)
+	{
+		float result = areaOfCircle(radius);
+		printf("Area of r =%f: %f\n", radius, result);
+	}
 }
